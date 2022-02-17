ebefa8ecf8f7c20e2ddaf3a86f4244fed514a8d8
foadtkf <fakhrulif@gmail.com>
2022-02-17 15:36:34 +0600

    fifth-commit

--- dom.js
+++ dom.js
@@ -10,9 +10,20 @@ function strcheck(name) {
         return true;
 
 }
+// function to check whether its a negative number or not
+function numcheck(name) {
+    if (parseFloat(element(name).value) < 0)
+        return false;
+    else
+        return true;
+}
+// Event for "calculate" button
 element('calculate').addEventListener('click', function() {
     if (!strcheck('income') || !strcheck('exp-food') || !strcheck('exp-rent') || !strcheck('exp-cloth'))
         alert('Please enter number');
+    else if (!numcheck('income') || !numcheck('exp-food') || !numcheck('exp-rent') || !numcheck('exp-cloth'))
+        alert('Value cannot be negative');
+
     else {
         const income = parseFloat(element('income').value);
         const food = parseFloat(element('exp-food').value);
@@ -30,16 +41,23 @@ element('calculate').addEventListener('click', function() {
     }
 })
 
+// Event for "save" button
 element('save').addEventListener('click', function() {
-    const sval = parseFloat(element('save-percent').value);
-    const income = parseFloat(element('income').value);
-    const balance = parseFloat(element('balance').innerText);
-    const temp = (income / 100) * sval;
-    if (temp > balance) {
-        alert('savings cannot be greater than balance');
-        element('save-percent').value = '';
-    } else {
-        element('savings').innerText = temp;
-        element('remainings').innerText = balance - temp;
+    if (!strcheck('save-percent'))
+        alert('Please enter number');
+    else if (!numcheck('save-percent'))
+        alert('Value cannot be negative');
+    else {
+        const sval = parseFloat(element('save-percent').value);
+        const income = parseFloat(element('income').value);
+        const balance = parseFloat(element('balance').innerText);
+        const temp = (income / 100) * sval;
+        if (temp > balance) {
+            alert('savings cannot be greater than balance');
+            element('save-percent').value = '';
+        } else {
+            element('savings').innerText = temp;
+            element('remainings').innerText = balance - temp;
+        }
     }
 })
