83667f1623c767f9f8dac98397d4c2c9e307e8fb
foadtkf <fakhrulif@gmail.com>
2022-02-17 14:55:11 +0600

    fourth-commit

--- dom.js
+++ dom.js
@@ -1,20 +1,32 @@
+// function to return an element by id
 function element(name) {
     return document.getElementById(name);
 }
+// function to check whether its a string or not
+function strcheck(name) {
+    if (isNaN(element(name).value))
+        return false;
+    else
+        return true;
 
+}
 element('calculate').addEventListener('click', function() {
-    const income = parseFloat(element('income').value);
-    const food = parseFloat(element('exp-food').value);
-    const rent = parseFloat(element('exp-rent').value);
-    const cloth = parseFloat(element('exp-cloth').value);
-    const total = food + rent + cloth;
-    if (total > income) {
-        alert('expense cannot be greater than income')
-        element('exp-total').innerText = 0;
-        element('balance').innerText = 0;
-    } else {
-        element('exp-total').innerText = total;
-        element('balance').innerText = income - total;
+    if (!strcheck('income') || !strcheck('exp-food') || !strcheck('exp-rent') || !strcheck('exp-cloth'))
+        alert('Please enter number');
+    else {
+        const income = parseFloat(element('income').value);
+        const food = parseFloat(element('exp-food').value);
+        const rent = parseFloat(element('exp-rent').value);
+        const cloth = parseFloat(element('exp-cloth').value);
+        const total = food + rent + cloth;
+        if (total > income) {
+            alert('expense cannot be greater than income')
+            element('exp-total').innerText = 0;
+            element('balance').innerText = 0;
+        } else {
+            element('exp-total').innerText = total;
+            element('balance').innerText = income - total;
+        }
     }
 })
 
